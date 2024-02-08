d5688a65bd01fa20d551637f791fb54aaf0bc009
Jack Kelly <jack@openclimatefix.org>
2024-02-08 17:06:36 +0000

    Use ManuallDrop::take instead of ManuallDrop::drop. Works well!

--- manuallydrop/src/main.rs
+++ manuallydrop/src/main.rs
@@ -17,25 +17,30 @@ fn main() {
     let ptr;
 
     {
-        let mut foo = ManuallyDrop::new(Foo {
+        let mut inner_foo = ManuallyDrop::new(Foo {
             i: 42,
             s: "hello!".to_string(),
         });
-        println!("original: {:?}", *foo);
+        println!(" original: {:?}", *inner_foo);
 
-        ptr = &mut foo as *mut ManuallyDrop<Foo>;
+        ptr = &mut inner_foo as *mut ManuallyDrop<Foo>;
         unsafe {
-            println!("   inner: {:?}", **ptr);
+            println!("inner ptr: {:?}", **ptr);
         }
 
         // If we don't use `ManuallyDrop` then `foo` is dropped here.
     }
 
+    let outer_foo: Foo;
     unsafe {
-        println!("   outer: {:?}", **ptr);
+        println!("outer ptr: {:?}", **ptr);
 
         // If the following line is commented out then `heaptrack` detects a memory leak
         // and `drop` isn't called.
-        ManuallyDrop::drop(&mut *ptr);
+        outer_foo = ManuallyDrop::take(&mut *ptr);
+
+        // Instead of `ManuallyDrop::take`, we could call:
+        // ManuallyDrop::drop(&mut *ptr);
     }
-}
+    println!("outer foo: {:?}", outer_foo);
+} // `outer_foo` is dropped here.
