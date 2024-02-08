873499ada72a239434cf0329748d688f0779d8b6
Jack Kelly <jack@openclimatefix.org>
2024-02-08 16:55:27 +0000

    ManuallyDrop works!

---
+++ manuallydrop/README.md
@@ -0,0 +1,7 @@
+Quick experiment into `ManuallyDrop`.
+
+To detect memory leaks:
+
+1. sudo apt install heaptrack
+2. cargo run
+3. heaptrack target/debug/manuallydrop
--- manuallydrop/src/main.rs
+++ manuallydrop/src/main.rs
@@ -1,3 +1,41 @@
+use std::mem::ManuallyDrop;
+
+#[derive(Debug)]
+struct Foo {
+    i: u64,
+    s: String,
+}
+
+impl Drop for Foo {
+    fn drop(&mut self) {
+        // Implement `drop` so we can see when `Foo` is dropped!
+        println!("DROP FOO!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let ptr;
+
+    {
+        let mut foo = ManuallyDrop::new(Foo {
+            i: 42,
+            s: "hello!".to_string(),
+        });
+        println!("original: {:?}", *foo);
+
+        ptr = &mut foo as *mut ManuallyDrop<Foo>;
+        unsafe {
+            println!("   inner: {:?}", **ptr);
+        }
+
+        // If we don't use `ManuallyDrop` then `foo` is dropped here.
+    }
+
+    unsafe {
+        println!("   outer: {:?}", **ptr);
+
+        // If the following line is commented out then `heaptrack` detects a memory leak
+        // and `drop` isn't called.
+        ManuallyDrop::drop(&mut *ptr);
+    }
 }
