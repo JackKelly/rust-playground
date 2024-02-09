e21ae3b9e8bb4b513d145df71f9c53a1dd461518
Jack Kelly <jack@openclimatefix.org>
2024-02-09 14:34:32 +0000

    It now works in release mode by using Box::into_raw

--- manuallydrop/README.md
+++ manuallydrop/README.md
@@ -1,11 +1,13 @@
 # ManuallyDrop
 
-Quick experiment exploring [`ManuallyDrop`](https://doc.rust-lang.org/std/mem/struct.ManuallyDrop.html).
+Quick experiment exploring [`ManuallyDrop`](https://doc.rust-lang.org/std/mem/struct.ManuallyDrop.html) and `Box::into_raw`.
+
+It turns out that [my original attempt to use `ManuallyDrop`](https://github.com/JackKelly/rust-playground/blob/d5688a65bd01fa20d551637f791fb54aaf0bc009/manuallydrop/src/main.rs) to keep an object alive after it goes out of scope, failed for the `String` member of `Foo` when compiled in `release` mode. I tried all the different combinations of `ManuallyDrop` and `Pin` that I could think of, but none worked! The only thing I could get to work was using `Box::into_raw`.
 
 To detect memory leaks:
 
 ```shell
-cargo build
+cargo build --release
 sudo apt install heaptrack
-heaptrack target/debug/manuallydrop
+heaptrack target/release/manuallydrop
 ```
--- manuallydrop/src/main.rs
+++ manuallydrop/src/main.rs
@@ -1,5 +1,3 @@
-use std::mem::ManuallyDrop;
-
 #[derive(Debug)]
 struct Foo {
     i: u64,
@@ -17,30 +15,29 @@ fn main() {
     let ptr;
 
     {
-        let mut inner_foo = ManuallyDrop::new(Foo {
+        let inner_foo = Box::new(Foo {
             i: 42,
             s: "hello!".to_string(),
         });
         println!(" original: {:?}", *inner_foo);
 
-        ptr = &mut inner_foo as *mut ManuallyDrop<Foo>;
+        ptr = Box::into_raw(inner_foo);
         unsafe {
-            println!("inner ptr: {:?}", **ptr);
+            println!("inner ptr: {:?}", *ptr);
         }
-
-        // If we don't use `ManuallyDrop` then `foo` is dropped here.
     }
 
-    let outer_foo: Foo;
+    let outer_foo: Box<Foo>;
     unsafe {
-        println!("outer ptr: {:?}", **ptr);
+        println!("outer ptr: {:?}", *ptr);
+
+        outer_foo = Box::from_raw(ptr);
 
         // If the following line is commented out then `heaptrack` detects a memory leak
         // and `drop` isn't called.
-        outer_foo = ManuallyDrop::take(&mut *ptr);
 
         // Instead of `ManuallyDrop::take`, we could call:
         // ManuallyDrop::drop(&mut *ptr);
     }
-    println!("outer foo: {:?}", outer_foo);
+    println!("outer_foo: {:?}", outer_foo);
 } // `outer_foo` is dropped here.
