7604b3f07c400daebbf8433b505c607604ae3183
Jack Kelly <jack@openclimatefix.org>
2024-02-21 10:53:28 +0000

    Using Tracker and TrackerWithBox. Doesn't change the order of the results!

--- page-faults/src/main.rs
+++ page-faults/src/main.rs
@@ -1,4 +1,7 @@
+use crate::tracker::{Tracker, TrackerUsingBox};
 use std::thread;
+
+mod tracker;
 
 struct Foo {
     a: usize,
@@ -27,25 +30,8 @@ impl Foo {
 const N: usize = 1_000_000;
 const N_FOOS: usize = 4;
 
-fn just_a_vec_of_vecs() {
+fn tracker_without_box() {
     // perf stat (result from previous commit):
-    //     Performance counter stats for './target/release/page-faults':
-
-    //     320.26 msec task-clock                       #    0.998 CPUs utilized
-    //         36      context-switches                 #  112.410 /sec
-    //         4      cpu-migrations                   #   12.490 /sec
-    //     29,380      page-faults                      #   91.739 K/sec
-    // 1,135,048,532      cycles                           #    3.544 GHz
-    // 2,208,078,923      instructions                     #    1.95  insn per cycle
-    // 433,725,846      branches                         #    1.354 G/sec
-    // 1,263,417      branch-misses                    #    0.29% of all branches
-
-    // 0.321032341 seconds time elapsed
-
-    // 0.228956000 seconds user
-    // 0.088369000 seconds sys
-
-    // perf stat (new result, for this commit, using Vec<Vec<Foo>>)
     //     Performance counter stats for './target/release/page-faults':
 
     //     1,131.38 msec task-clock                       #    0.996 CPUs utilized
@@ -62,35 +48,38 @@ fn just_a_vec_of_vecs() {
     // 0.713242000 seconds user
     // 0.414397000 seconds sys
 
-    // Put N Foos into a Vec:
-    let vec: Vec<Vec<Foo>> = (0..N).map(|i| Foo::get_multiple_foos(i, N_FOOS)).collect();
+    // perf stat (new result, for this commit, using Tracker<Foo>)
+    //     Performance counter stats for './target/release/page-faults':
 
-    // Consume the vec:
-    for f in vec.into_iter() {
-        assert_eq!(f.len(), N_FOOS);
+    //     245.52 msec task-clock                       #    0.995 CPUs utilized
+    //         9      context-switches                 #   36.657 /sec
+    //         1      cpu-migrations                   #    4.073 /sec
+    //     31,334      page-faults                      #  127.624 K/sec
+    // 876,015,345      cycles                           #    3.568 GHz
+    // 1,484,094,734      instructions                     #    1.69  insn per cycle
+    // 289,492,663      branches                         #    1.179 G/sec
+    // 1,988,980      branch-misses                    #    0.69% of all branches
+
+    // 0.246733281 seconds time elapsed
+
+    // 0.172706000 seconds user
+    // 0.072295000 seconds sys
+
+    let mut tracker: Tracker<Foo> = Tracker::new(N);
+
+    for i in 0..N {
+        let index = tracker.get_next_index().unwrap();
+        tracker.put(index, Foo::new(i));
+    }
+
+    for i in 0..N {
+        tracker.remove(i).unwrap();
     }
     println!("DONE!");
 }
 
-fn vec_of_vec_of_boxes() {
+fn tracker_with_internal_boxes() {
     // perf stat (result from previous commit):
-    //     Performance counter stats for './target/release/page-faults':
-
-    //     419.55 msec task-clock                       #    0.998 CPUs utilized
-    //         12      context-switches                 #   28.602 /sec
-    //         1      cpu-migrations                   #    2.384 /sec
-    //     33,287      page-faults                      #   79.341 K/sec
-    // 1,483,567,429      cycles                           #    3.536 GHz
-    // 2,819,406,676      instructions                     #    1.90  insn per cycle
-    // 559,878,833      branches                         #    1.334 G/sec
-    // 3,574,667      branch-misses                    #    0.64% of all branches
-
-    // 0.420288255 seconds time elapsed
-
-    // 0.318706000 seconds user
-    // 0.100856000 seconds sys
-
-    // perf stat (new result, for this commit, using Vec<Vec<Box<Foo>>>)
     //     Performance counter stats for './target/release/page-faults':
 
     //     1,456.03 msec task-clock                       #    0.998 CPUs utilized
@@ -107,19 +96,37 @@ fn vec_of_vec_of_boxes() {
     // 1.017636000 seconds user
     // 0.434989000 seconds sys
 
-    // Put N Foos into a Vec:
-    let vec: Vec<Vec<Box<Foo>>> = (0..N)
-        .map(|i| Foo::get_multiple_boxed_foos(i, N_FOOS))
-        .collect();
+    // perf stat (new result, for this commit, using TrackerWithBox<Foo>)
+    //     Performance counter stats for './target/release/page-faults':
 
-    // Consume the vec:
-    for f in vec.into_iter() {
-        assert_eq!(f.len(), N_FOOS);
+    //     335.88 msec task-clock                       #    0.992 CPUs utilized
+    //         12      context-switches                 #   35.727 /sec
+    //         0      cpu-migrations                   #    0.000 /sec
+    //     35,240      page-faults                      #  104.918 K/sec
+    // 1,219,417,371      cycles                           #    3.630 GHz
+    // 2,192,701,091      instructions                     #    1.80  insn per cycle
+    // 432,072,896      branches                         #    1.286 G/sec
+    // 1,521,752      branch-misses                    #    0.35% of all branches
+
+    // 0.338726103 seconds time elapsed
+
+    // 0.150666000 seconds user
+    // 0.182386000 seconds sys
+
+    let mut tracker: TrackerUsingBox<Foo> = TrackerUsingBox::new(N);
+
+    for i in 0..N {
+        let index = tracker.get_next_index().unwrap();
+        tracker.put(index, Foo::new(i));
+    }
+
+    for i in 0..N {
+        tracker.remove(i).unwrap();
     }
     println!("DONE!");
 }
 
 fn main() {
-    let t = thread::spawn(vec_of_vec_of_boxes);
+    let t = thread::spawn(tracker_without_box);
     t.join().unwrap();
 }
