28f9912b9317bd6b4799e744d542ff1ea387d06a
Jack Kelly <jack@openclimatefix.org>
2024-03-13 20:44:26 +0000

    Yay! ThreadLocal works!

--- rayon-for-io-threadpool/src/main.rs
+++ rayon-for-io-threadpool/src/main.rs
@@ -1,6 +1,7 @@
 use rayon::iter::{IntoParallelIterator, ParallelBridge};
 use rayon::prelude::ParallelIterator;
 use std::sync::mpsc::channel;
+use thread_local::ThreadLocal;
 
 fn main() {
     let (in_tx, in_rx) = channel();
@@ -9,26 +10,29 @@ fn main() {
         .num_threads(2)
         .build()
         .unwrap();
+    let tls = ThreadLocal::new();
 
     pool.spawn(move || {
-        in_rx.into_iter().par_bridge().for_each_init(
-            || {
-                println!("outer init");
+        in_rx.into_iter().par_bridge().for_each(|elem| {
+            // Let's pretend that we've got some additional operations
+            // to add to the task list:
+            if elem < 5 {
+                (0..4).into_par_iter().for_each(|i| {
+                    let out_tx2 = tls.get_or(|| {
+                        println!("Creating new out_tx clone (inner)!");
+                        out_tx.clone()
+                    });
+                    out_tx2.send(i + elem + 100).unwrap()
+                });
+            }
+
+            // We're ready to send a results back:
+            let out_tx2 = tls.get_or(|| {
+                println!("Creating new out_tx clone!");
                 out_tx.clone()
-            },
-            |out_tx, elem| {
-                if elem < 5 {
-                    (0..4).into_par_iter().for_each_init(
-                        || {
-                            println!("inner init");
-                            out_tx.clone()
-                        },
-                        |out_tx, i| out_tx.send(i + elem + 100).unwrap(),
-                    );
-                }
-                out_tx.send(elem * 10).unwrap()
-            },
-        );
+            });
+            out_tx2.send(elem * 10).unwrap()
+        });
     });
 
     for i in 0..32 {
