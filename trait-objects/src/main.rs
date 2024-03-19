2aa8044a1b0bd00f5f577804dd55f7daa925761f
Jack Kelly <jack@openclimatefix.org>
2024-03-19 21:31:50 +0000

    feels like we're onto something with separate MergedGetOp and SplitGetOp. Need to think how best to ensure we can reuse the scheduling code between IO backends.

--- trait-objects/src/main.rs
+++ trait-objects/src/main.rs
@@ -9,6 +9,28 @@ use std::sync::Arc;
 
 trait OperationMarker: Debug {}
 
+enum OptimisedGetRanges<M> {
+    Unchanged {
+        filename: Arc<PathBuf>,
+        byte_range: Range<isize>,
+        metadata: Option<M>,
+    },
+    // A single (large) user op which has been split into smaller ops:
+    Split {
+        filename: Arc<PathBuf>,
+        split_byte_ranges: Vec<Range<isize>>,
+        user_byte_range: Range<isize>,
+        user_metadata: M,
+    },
+    // Multiple small user ops which have been merged into a single op:
+    Merged {
+        filename: Arc<PathBuf>,
+        merged_byte_range: Range<isize>,
+        user_byte_ranges: Vec<Range<isize>>,
+        user_metadata: Vec<M>,
+    },
+}
+
 trait GetRanges<M> {
     /// `byte_range`:
     /// The byte range for the file. Negative numbers are relative to the filesize.
@@ -37,6 +59,19 @@ trait GetRanges<M> {
         byte_ranges: Vec<Range<isize>>,
         metadata: Option<Vec<M>>,
     ) -> Self;
+
+    // Call some combination of merge and split:
+    fn optimise(&self) -> Vec<OptimisedGetRanges<M>>;
+
+    fn merge(&self, max_gap: usize) -> Vec<OptimisedGetRanges<M>> {
+        // TODO: Provide default impl
+        todo!();
+    }
+
+    fn split(&self, max_file_size: usize) -> Vec<OptimisedGetRanges<M>> {
+        // TODO: Provide default impl
+        todo!();
+    }
 }
 
 ///------------ CODE THAT'S SPECIFIC TO A SINGLE I/O BACKEND --------
@@ -52,23 +87,6 @@ struct GetRangesUserOp<M> {
 
 impl<M> OperationMarker for GetRangesUserOp<M> where M: Debug {}
 
-// Let's say the user asks for one 4 GByte file.
-// Linux cannot load anything larger than 2 GB in one go.
-// But we don't know the size immediately because the user used byte_range=0..-1.
-// The steps will be:
-// 1. Get the filesize from io_uring and, concurrently, open the file.
-// 2. As soon as the filesize is returned, create one 4 GB buffer.
-// 3. Set opt_byte_ranges to [0..2GB, 2GB..4GB].
-// 4. Set opt_buffer_offsets to [0, 2GB].
-// 5. Set opt_to_user to [0, 0].
-// 6. Once the `open` operatoin has completed, submit both `read` operations.
-// 7. When both optimised `read` operations have completed, submit the user_buffer and metadata to the
-//    completion queue.
-//
-// Now let's say that the user asks for 1 million chunks from a single file.
-// Some of these chunks are close, so we merge them.
-// The user has asked for only 1,000 buffers to be allocated at any given time.
-//
 impl<M> GetRanges<M> for GetRangesUserOp<M> {
     fn get_ranges(
         filename: PathBuf,
@@ -87,9 +105,50 @@ impl<M> GetRanges<M> for GetRangesUserOp<M> {
     }
 }
 
+// Once the GetRangesUserOp passes to the uring threadpool,
+// the first worker thread which grabs this GetRangesUserOp
+// will get the filesize (if necessary) and then optimise the byte_ranges and submit some
+// combination of
+// `UnchangedGetOp`, `MergedGetOp`, and `SplitGetOp` to the Rayon
+// task queue. Each of these will implement the `UringTask` trait
+// (which has methods for `process_cqe` and `next`).
+//
+// for example:
+//
+// Let's say the user asks for one 4 GByte file.
+// Linux cannot load anything larger than 2 GB in one go.
+// But we don't know the size immediately because the user used byte_range=0..-1.
+// The steps will be:
+// 1. Get the filesize from io_uring and, concurrently, open the file.
+// 2. As soon as the filesize is returned, we see that this is a big file, and so we need to submit
+//    a `SplitGetOp`.
+// 3. Set split_byte_ranges to [0..2GB, 2GB..4GB].
+// 4. Set next_to_submit to 0, and set n_completed to 0.
+// 5. Submit the uring operations, using the appropriate pointer offset.
+// 7. When both `read` operations have completed, submit the user_buffer and metadata to the
+//    completion queue.
+//
+// Now let's say that the user asks for 1 million chunks from a single file.
+// Some of these chunks are close, so we merge them.
+// The user has asked for only 1,000 buffers to be allocated at any given time.
+// 1. The first uring worker thread gets the filesize if necessary.
+// 2. Then submits a mix of operations to the Rayon task queue.
+// 3. Each MergedGetOp just submits a single read to uring,
+// and when that read completes, it submits read-only slices (but how to keep the buffer alive, if
+// we're only passing back &[u8]? Maybe use Bytes?).
+
+struct UnchangedGetOp<M> {
+    filename: Arc<PathBuf>,
+    byte_range: Range<isize>,
+    buffer: Option<Vec<u8>>,
+    metadata: Option<M>,
+}
+
 // A single user operation has been split into multiple get operations:
 // For example, a user submitted a 4 GByte file, but Linux cannot read
-// more than 2 GB at once:
+// more than 2 GB at once.
+// Each `SplitGetOp` will be processed by only one uring thread, so we don't
+// need any locks.
 #[derive(Debug)]
 struct SplitGetOp<M> {
     filename: Arc<PathBuf>,
