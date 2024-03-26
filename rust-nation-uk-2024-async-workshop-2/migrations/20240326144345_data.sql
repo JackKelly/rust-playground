2e60dcfe914fe390e470de509a384c1a84179c69
Jack Kelly <jack@openclimatefix.org>
2024-03-26 15:19:55 +0000

    Using Axum Router

---
+++ rust-nation-uk-2024-async-workshop-2/migrations/20240326144345_data.sql
@@ -0,0 +1,5 @@
+-- Add migration script here
+INSERT INTO blog_posts (date, title, body, author) VALUES
+	('2021-01-01', 'A Tale of Two Cities', 'It was the best of times, it was the worst of times.', 'Foo'),
+	('2021-01-02', 'Moby Dick', 'Call me Ishmael.', 'Melville');
+
