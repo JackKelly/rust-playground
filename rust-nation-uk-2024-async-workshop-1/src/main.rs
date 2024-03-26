8ce512bcce4bd140019706111b5538847299fc64
Jack Kelly <jack@openclimatefix.org>
2024-03-26 12:03:29 +0000

    Use Rayon. Tiny speedup: 0.54 sec vs 0.55 sec

--- rust-nation-uk-2024-async-workshop-1/src/main.rs
+++ rust-nation-uk-2024-async-workshop-1/src/main.rs
@@ -1,6 +1,4 @@
-use rand::seq::SliceRandom;
-use std::thread;
-use std::time::Duration;
+use rayon::prelude::*;
 use std::time::Instant;
 
 const MAX_NUMBER: usize = 100_000;
@@ -20,47 +18,17 @@ fn is_prime(n: usize) -> bool {
 }
 
 fn main() {
-    let mut candidates: Vec<usize> = (0..MAX_NUMBER).collect();
+    let candidates: Vec<usize> = (0..MAX_NUMBER).collect();
     // Perform the calculation
     let start = Instant::now(); // We're not timing the initial creation
+                                //
 
-    // Shuffle, so that the work is more evenly distributed between threads:
-    candidates.shuffle(&mut rand::thread_rng());
-    let mut primes: Vec<usize> = Vec::with_capacity(10_000);
+    let primes = candidates
+        .par_iter()
+        .filter(|n| is_prime(**n))
+        .map(|n| *n)
+        .collect::<Vec<usize>>();
 
-    let mut extend_time = Duration::ZERO;
-    thread::scope(|scope| {
-        let mut handles = Vec::with_capacity(num_cpus::get());
-
-        let chunks = candidates.chunks(candidates.len() / num_cpus::get());
-
-        // Iterate each chunk
-        for (id, chunk) in chunks.enumerate() {
-            println!("Thread #{id} is using chunk size: {}", chunk.len());
-            let handle = scope.spawn(move || {
-                let chunk_start = Instant::now();
-
-                let local_results: Vec<usize> =
-                    chunk.iter().filter(|n| is_prime(**n)).map(|n| *n).collect();
-
-                let chunk_elapsed = chunk_start.elapsed();
-                println!(
-                    "Thread #{id} took {:.4} seconds.",
-                    chunk_elapsed.as_secs_f32()
-                );
-                local_results
-            });
-
-            handles.push(handle);
-        }
-
-        for handle in handles {
-            let local_result: Vec<usize> = handle.join().unwrap();
-            let extend_start = Instant::now();
-            primes.extend(local_result);
-            extend_time += extend_start.elapsed();
-        }
-    });
     // Time how long it took
     let elapsed = start.elapsed();
 
@@ -71,8 +39,4 @@ fn main() {
         candidates.len()
     );
     println!("Calculated in {:.4} seconds.", elapsed.as_secs_f32());
-    println!(
-        "Vec::extend() took a total of: {:.4} microsecs.",
-        extend_time.as_micros()
-    );
 }
