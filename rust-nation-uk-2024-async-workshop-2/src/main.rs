b7ce60f45660e45069b0574ca601444dd7bdde98
Jack Kelly <jack@openclimatefix.org>
2024-03-26 14:12:50 +0000

    using .env

--- rust-nation-uk-2024-async-workshop-2/src/main.rs
+++ rust-nation-uk-2024-async-workshop-2/src/main.rs
@@ -2,6 +2,9 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("Hello, world!");
+    // Read the .env file and apply it
+    dotenvy::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL")?;
+    println!("Connecting to: {database_url}");
     Ok(())
 }
