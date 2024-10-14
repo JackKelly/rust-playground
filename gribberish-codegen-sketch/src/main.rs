649bba83ffde87b82b637cb9f0015a23cf2fc0b2
Jack Kelly <jack@openclimatefix.org>
2024-10-14 18:10:53 +0100

    Tinkering with builder pattern

--- gribberish-codegen-sketch/src/main.rs
+++ gribberish-codegen-sketch/src/main.rs
@@ -1,31 +1,45 @@
+/// Identification of originating/generating center.
+enum Center {}
+
 #[derive(num_derive::FromPrimitive)]
 enum Discipline {
     Meteorological = 0,
+    Hydrological = 1,
     // etc.
 }
 
+trait ProductMarker {}
+
+impl Discipline {
+    fn category(&self, category_number: u8) -> Box<dyn ProductMarker> {
+        match self {
+            Discipline::Meteorological => {
+                Box::new(MeteorologicalProduct::from_u8(category_number).unwrap())
+            }
+            Discipline::Hydrological => {
+                Box::new(HydrologicalProduct::from_u8(category_number).unwrap())
+            }
+        }
+    }
+}
+
 #[derive(num_derive::FromPrimitive)]
-enum Category {
+enum MeteorologicalProduct {
     Temperature = 0,
     Moisture = 1,
     // etc.
 }
 
-impl Category {
-    fn discipline(&self) -> Discipline {
-        match *self {
-            Category::Temperature => Discipline::Meteorological,
-            Category::Moisture => Discipline::Meteorological,
-        }
-    }
+#[derive(num_derive::FromPrimitive)]
+enum HydrologicalProduct {
+    HydrologyBasicProduct = 0,
 }
 
-// The `Parameter` enum variant names would be derived from the `name` field in the GDAL CSVs,
-// with the strings turned into CammelCase, and the whitespace removed, and ignoring rows where
-// the `name` contains "Reserved". Maybe this single enum would contain _all_ parameters
-// (for all categories)?
+impl ProductMarker for MeteorologicalProduct {}
+impl ProductMarker for HydrologicalProduct {}
+
 #[derive(num_derive::FromPrimitive)]
-enum Parameter {
+enum Temperature {
     Temperature,
     VirtualTemperature,
     PotentialTemperature,
@@ -38,107 +52,88 @@ enum Parameter {
     // etc.
 }
 
-use Parameter::*;
+trait Parameter {}
 
-impl Parameter {
+impl Parameter for Temperature;
+
+impl Temperature {
     #[no_mangle] // Required when viewing this in godbolt.org
     pub fn abbrev(&self) -> &'static str {
         // This gets compiled to a jump table, which is O(1). See:
         // https://www.reddit.com/r/rust/comments/31kras/are_match_statements_constanttime_operations/
         match *self {
-            Temperature => "TMP",
-            VirtualTemperature => "VTMP",
-            PotentialTemperature => "POT",
-            PseudoAdiabaticPotentialTemperature => "EPOT",
-            MaximumTemperature => "TMAX",
-            MinimumTemperature => "TMIN",
-            DewPointTemperature => "DPT",
-            DewPointDepression => "DEPR",
-            LapseRate => "LAPR",
+            Temperature::Temperature => "TMP",
+            Temperature::VirtualTemperature => "VTMP",
+            Temperature::PotentialTemperature => "POT",
+            Temperature::PseudoAdiabaticPotentialTemperature => "EPOT",
+            Temperature::MaximumTemperature => "TMAX",
+            Temperature::MinimumTemperature => "TMIN",
+            Temperature::DewPointTemperature => "DPT",
+            Temperature::DewPointDepression => "DEPR",
+            Temperature::LapseRate => "LAPR",
             // etc.
         }
     }
 
     pub fn name(&self) -> &'static str {
         match *self {
-            Temperature => "Temperature",
-            VirtualTemperature => "Virtual temperature",
+            Temperature::Temperature => "Temperature",
+            Temperature::VirtualTemperature => "Virtual temperature",
             _ => todo!(), // etc...
         }
     }
 
-    pub fn category(&self) -> Category {
-        match *self {
-            Temperature | VirtualTemperature | PotentialTemperature => Category::Temperature,
-            _ => todo!(),
-        }
+    pub fn unit(&self) -> &'static str {
+        todo!();
     }
 }
 
 // `phf::Map` is compiled to a perfect hash table, which is O(1). In contrast,
 // matching strings compiles code which checks each string in turn, which is O(n).
-static ABBREV_TO_PRODUCT_VARIANT: phf::Map<&'static str, Parameter> = phf::phf_map! {
-    "TMP" => Temperature,
-    "VTMP" => VirtualTemperature,
-    "POT" => PotentialTemperature,
-    "EPOT" => PseudoAdiabaticPotentialTemperature,
-    "TMAX" => MaximumTemperature,
-    "TMIN" => MinimumTemperature,
-    "DPT" => DewPointTemperature,
-    "DEPR" => DewPointDepression,
-    "LAPR" => LapseRate,
+// TODO: Maybe we do need a super-struct of all Parameters?
+static ABBREV_TO_PRODUCT_VARIANT: phf::Map<&'static str, Box<dyn Parameter>> = phf::phf_map! {
+    "TMP" => Box::new(Temperature::Temperature),
+    "VTMP" => Box::new(Temperature::VirtualTemperature),
+    "POT" => Box::new(Temperature::PotentialTemperature),
+    "EPOT" => Box::new(Temperature::PseudoAdiabaticPotentialTemperature),
+    "TMAX" => Box::new(Temperature::MaximumTemperature),
+    "TMIN" => Box::new(Temperature::MinimumTemperature),
+    "DPT" => Box::new(Temperature::DewPointTemperature),
+    "DEPR" => Box::new(Temperature::DewPointDepression),
+    "LAPR" => Box::new(Temperature::LapseRate),
 };
 
-// struct Key(u32);
-//
-// impl Key {
-//     const fn new(discipline: u8, category: u8, parameter: u8) -> Self {
-//         let mut key: u32 = discipline as u32;
-//         key |= (category as u32) << 8;
-//         key |= (parameter as u32) << 16;
-//         Self(key)
-//     }
-// }
-//
-
-struct Key {
-    discipline: u8,
-    category: u8,
-    parameter: u8,
-}
-
-impl From<Key> for Parameter {
-    fn from(value: Key) -> Self {
-        match value {
-            Key {
-                discipline: 0,
-                category: 0,
-                parameter: 0,
-            } => Temperature,
-            _ => todo!(),
-        }
-    }
-}
-
-impl std::str::FromStr for &Parameter {
-    type Err = String;
-
-    #[no_mangle]
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match ABBREV_TO_PRODUCT_VARIANT.get(&s) {
-            Some(prod) => Ok(prod),
-            None => Err(format!("Unrecognised product abbreviation: {s}")),
-        }
-    }
-}
-
 fn main() {
-    // Convert abbreviation string to Parameter:
-    let abbrev = "TMP";
-    let param: &Parameter = abbrev.parse().unwrap();
+    // First, build a ParamDecoder:
+    let param_decoder: ParamDecoder = ParamDecoderBuilder::new()
+        .master_tables_version_number(24) // Optional. Defaults to latest table version.
+        .with_local_tables_version_number(4) // Optional. Defaults to ignoring local tables.
+        .center(Center::NCEP) // Required iff local tables are used.
+        .build();
 
-    // Get other information about the parameter:
-    let name = param.name();
-    let category = param.category();
-    let discipline = category.discipline();
+    // Now, there are two ways to "decode" a parameter:
+    // 1) Decode from the binary data in the GRIB sections:
+    let param = param_decoder
+        .from_grib() // -> ParamDecoderFromGrib
+        .discipline(Discipline::from_u8(0)) // or Discipline::from_u8(0)
+        .category(MeteorologicalProduct::from_u8(1)) //
+        .parameter_number(0)
+        .build();
+
+    // 2) Or decode from the abbreviation string from an .idx file:
+    let param = param_decoder
+        .from_idx() // -> ParamDecoderFromIdx
+        .abbrev("TMP")
+        .build();
+
+    // Now print information:
+    println!("{}, {}, {}", param.name(), param.unit(), param.abbrev());
+
+    // The above code is probably sufficient for the MVP. Later, we could add things like:
+    let param = param_decoder
+        .from_idx() // -> ParamDecoderFromIdx
+        .abbrev("TMP")
+        .vertical_level("5000-2000 m above ground")
+        .step("775-780 min ave fcst")
+        .build();
 }
