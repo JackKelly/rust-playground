217cd32fd798c5ccf66caaf527ed711a1dd94bf1
Jack Kelly <jack@openclimatefix.org>
2024-10-15 11:49:03 +0100

    Update names and comments

--- gribberish-codegen-sketch/src/main.rs
+++ gribberish-codegen-sketch/src/main.rs
@@ -17,26 +17,26 @@ struct CenterAndTableVersions {
     master_table_version: MasterTableVersion,
 }
 
-enum Discipline {
-    Meteorological(MeteorologicalProduct),
-    Hydrological(HydrologicalProduct),
+enum Product {
+    Meteorological(MeteorologicalCategory),
+    Hydrological(HydrologicalCategory),
 
     // Local to NCEP:
     NcepFoo(NcepFooProduct),
     // etc.
 }
 
-impl Discipline {
+impl Product {
     pub fn from_discipline_and_category_and_parameter_numbers(
         discipline_num: u8,
         category_num: u8,
         parameter_num: u8,
         center_and_table_versions: CenterAndTableVersions,
-    ) -> Option<Discipline> {
+    ) -> Option<Product> {
         // This function just routes the query to the functions which handle Disciplines specified
         // in either local or master tables.
         match discipline_num {
-            ..192 => Discipline::from_master_discipline_and_category_and_parameter_numbers(
+            ..192 => Product::from_master_discipline_and_category_and_parameter_numbers(
                 discipline_num,
                 category_num,
                 parameter_num,
@@ -44,7 +44,7 @@ impl Discipline {
             ),
 
             // Reserved for local use:
-            192..=254 => Discipline::from_local_discipline_and_category_and_parameter_numbers(
+            192..=254 => Product::from_local_discipline_and_category_and_parameter_numbers(
                 discipline_num,
                 category_num,
                 parameter_num,
@@ -60,10 +60,10 @@ impl Discipline {
         category_num: u8,
         parameter_num: u8,
         center_and_table_versions: CenterAndTableVersions,
-    ) -> Option<Discipline> {
+    ) -> Option<Product> {
         match center_and_table_versions.center {
             Center::NCEP => match discipline_num {
-                192 => Some(Discipline::NcepFoo(
+                192 => Some(Product::NcepFoo(
                     NcepFooProduct::from_category_and_parameter_numbers(
                         category_num,
                         parameter_num,
@@ -81,8 +81,8 @@ impl Discipline {
         center_and_table_versions: CenterAndTableVersions,
     ) -> Option<Self> {
         match discipline_num {
-            0 => Some(Discipline::Meteorological(
-                MeteorologicalProduct::from_category_and_parameter_numbers(
+            0 => Some(Product::Meteorological(
+                MeteorologicalCategory::from_category_and_parameter_numbers(
                     category_num,
                     parameter_num,
                     center_and_table_versions,
@@ -103,7 +103,7 @@ impl Discipline {
     }
 }
 
-trait Product {
+trait Category {
     fn from_category_and_parameter_numbers(
         category_num: u8,
         parameter_num: u8,
@@ -113,13 +113,13 @@ trait Product {
         Self: Sized;
 }
 
-enum MeteorologicalProduct {
-    Temperature(Temperature),
-    Moisture(Moisture),
+enum MeteorologicalCategory {
+    Temperature(TemperatureParameter),
+    Moisture(MoistureParameter),
     // etc.
 }
 
-impl Product for MeteorologicalProduct {
+impl Category for MeteorologicalCategory {
     fn from_category_and_parameter_numbers(
         category_num: u8,
         parameter_num: u8,
@@ -129,23 +129,27 @@ impl Product for MeteorologicalProduct {
         Self: Sized,
     {
         match category_num {
-            0 => Some(MeteorologicalProduct::Temperature(
-                Temperature::from_parameter_num(parameter_num, center_and_table_versions)?,
+            0 => Some(MeteorologicalCategory::Temperature(
+                TemperatureParameter::from_parameter_num(parameter_num, center_and_table_versions)?,
             )),
-            1 => Some(MeteorologicalProduct::Moisture(
-                Moisture::from_parameter_num(parameter_num, center_and_table_versions)?,
+            1 => Some(MeteorologicalCategory::Moisture(
+                MoistureParameter::from_parameter_num(parameter_num, center_and_table_versions)?,
             )),
             _ => None,
         }
     }
 }
 
-enum HydrologicalProduct {
+enum HydrologicalCategory {
     HydrologyBasicProduct, // TODO: Add embedded enum
 }
 
-impl Product for HydrologicalProduct {
-    fn from_category_and_parameter_numbers(category_num: u8, parameter_num: u8) -> Option<Self>
+impl Category for HydrologicalCategory {
+    fn from_category_and_parameter_numbers(
+        category_num: u8,
+        parameter_num: u8,
+        center_and_table_versions: CenterAndTableVersions,
+    ) -> Option<Self>
     where
         Self: Sized,
     {
@@ -154,7 +158,7 @@ impl Product for HydrologicalProduct {
 }
 
 trait Parameter {
-    fn from_parameter_number(
+    fn from_parameter_num(
         parameter_num: u8,
         center_and_table_versions: CenterAndTableVersions,
     ) -> Option<Self>
@@ -167,7 +171,7 @@ trait Parameter {
 }
 
 #[derive(FromPrimitive)]
-enum Temperature {
+enum TemperatureParameter {
     Temperature = 0,
     VirtualTemperature,
     PotentialTemperature,
@@ -186,10 +190,10 @@ enum Temperature {
 }
 
 #[derive(FromPrimitive)]
-enum Moisture {}
+enum MoistureParameter {}
 
-impl Parameter for Temperature {
-    fn from_parameter_number(
+impl Parameter for TemperatureParameter {
+    fn from_parameter_num(
         parameter_num: u8,
         center_and_table_versions: CenterAndTableVersions,
     ) -> Option<Self>
@@ -197,13 +201,13 @@ impl Parameter for Temperature {
         Self: Sized,
     {
         if parameter_num < 192 {
-            Temperature::from_u8(parameter_num)
+            TemperatureParameter::from_u8(parameter_num)
         } else {
             // Parameter numbers >= 194 are reserved for local use:
             match center_and_table_versions.center {
                 Center::NCEP => match parameter_num {
-                    192 => Some(Temperature::NcepSnowPhaseChangeHeatFlux),
-                    193 => Some(Temperature::NcepTemperatureTendencyByAllRadiation),
+                    192 => Some(TemperatureParameter::NcepSnowPhaseChangeHeatFlux),
+                    193 => Some(TemperatureParameter::NcepTemperatureTendencyByAllRadiation),
                     _ => todo!(),
                 },
             }
@@ -214,27 +218,27 @@ impl Parameter for Temperature {
         // This gets compiled to a jump table, which is O(1). See:
         // https://www.reddit.com/r/rust/comments/31kras/are_match_statements_constanttime_operations/
         match *self {
-            Temperature::Temperature => "TMP",
-            Temperature::VirtualTemperature => "VTMP",
-            Temperature::PotentialTemperature => "POT",
-            Temperature::PseudoAdiabaticPotentialTemperature => "EPOT",
-            Temperature::MaximumTemperature => "TMAX",
-            Temperature::MinimumTemperature => "TMIN",
-            Temperature::DewPointTemperature => "DPT",
-            Temperature::DewPointDepression => "DEPR",
-            Temperature::LapseRate => "LAPR",
+            TemperatureParameter::Temperature => "TMP",
+            TemperatureParameter::VirtualTemperature => "VTMP",
+            TemperatureParameter::PotentialTemperature => "POT",
+            TemperatureParameter::PseudoAdiabaticPotentialTemperature => "EPOT",
+            TemperatureParameter::MaximumTemperature => "TMAX",
+            TemperatureParameter::MinimumTemperature => "TMIN",
+            TemperatureParameter::DewPointTemperature => "DPT",
+            TemperatureParameter::DewPointDepression => "DEPR",
+            TemperatureParameter::LapseRate => "LAPR",
             // etc.
 
             // Local to NCEP:
-            Temperature::NcepSnowPhaseChangeHeatFlux => "SNOHF",
-            Temperature::NcepTemperatureTendencyByAllRadiation => "TTRAD",
+            TemperatureParameter::NcepSnowPhaseChangeHeatFlux => "SNOHF",
+            TemperatureParameter::NcepTemperatureTendencyByAllRadiation => "TTRAD",
         }
     }
 
     fn name(&self) -> &'static str {
         match *self {
-            Temperature::Temperature => "Temperature",
-            Temperature::VirtualTemperature => "Virtual temperature",
+            TemperatureParameter::Temperature => "Temperature",
+            TemperatureParameter::VirtualTemperature => "Virtual temperature",
             _ => todo!(), // etc...
         }
     }
@@ -246,35 +250,38 @@ impl Parameter for Temperature {
 
 /// All the abbreviations which are common across all centers and all table versions.
 ///
+/// To decode .idx files, we need a single hashmap which holds every abbreviation string.
+/// So the values of the hashmap have to all be the same type.
+///
 /// `phf::Map` is compiled to a perfect hash table, which is O(1). In contrast,
 /// matching strings compiles code which checks each string in turn, which is O(n).
-static ABBREV_TO_PRODUCT_COMMON: phf::Map<&'static str, Discipline> = phf::phf_map! {
-    "TMP" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::Temperature)),
-    "VTMP" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::VirtualTemperature)),
-    "POT" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::PotentialTemperature)),
-    "EPOT" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::PseudoAdiabaticPotentialTemperature)),
-    "TMAX" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::MaximumTemperature)),
-    "TMIN" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::MinimumTemperature)),
-    "DPT" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::DewPointTemperature)),
-    "DEPR" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::DewPointDepression)),
-    "LAPR" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::LapseRate)),
+static ABBREV_TO_PRODUCT_COMMON: phf::Map<&'static str, Product> = phf::phf_map! {
+    "TMP" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::Temperature)),
+    "VTMP" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::VirtualTemperature)),
+    "POT" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::PotentialTemperature)),
+    "EPOT" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::PseudoAdiabaticPotentialTemperature)),
+    "TMAX" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::MaximumTemperature)),
+    "TMIN" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::MinimumTemperature)),
+    "DPT" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::DewPointTemperature)),
+    "DEPR" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::DewPointDepression)),
+    "LAPR" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::LapseRate)),
 };
 
-static ABBREV_TO_PRODUCT_NCEP: phf::Map<&'static str, Discipline> = phf::phf_map! {
-    "SNOHF" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::NcepSnowPhaseChangeHeatFlux)),
-    "TTRAD" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::NcepTemperatureTendencyByAllRadiation)),
+static ABBREV_TO_PRODUCT_NCEP: phf::Map<&'static str, Product> = phf::phf_map! {
+    "SNOHF" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::NcepSnowPhaseChangeHeatFlux)),
+    "TTRAD" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::NcepTemperatureTendencyByAllRadiation)),
 };
 
 // Contains only the diff between master table V32 and the common abbreviations.
-static ABBREV_TO_PRODUCT_MASTER_TABLE_V32: phf::Map<&'static str, Discipline> = phf::phf_map! {
-    "FOO" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::Foo)),
-    "BAR" => Discipline::Meteorological(MeteorologicalProduct::Temperature(Temperature::Bar)),
+static ABBREV_TO_PRODUCT_MASTER_TABLE_V32: phf::Map<&'static str, Product> = phf::phf_map! {
+    "FOO" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::Foo)),
+    "BAR" => Product::Meteorological(MeteorologicalCategory::Temperature(TemperatureParameter::Bar)),
 };
 
 pub fn abbrev_to_product(
     abbrev: &str,
     center_and_table_versions: &CenterAndTableVersions,
-) -> Option<&'static Discipline> {
+) -> Option<&'static Product> {
     // First, try to common abbreviations:
     if let Some(discipline) = ABBREV_TO_PRODUCT_COMMON.get(abbrev) {
         return Some(discipline);
@@ -296,8 +303,10 @@ pub fn abbrev_to_product(
 }
 
 // TODO: Think about the API for decoding vertical levels and steps from IDX files and from GRIB.
+//       Probably using GRIB Templates, e.g. https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_temp4-0.shtml
 // TODO: Split the code above into separate rust files.
 // TODO: Update this main function.
+// TODO: Link to this code from https://github.com/mpiannucci/gribberish/issues/63
 fn main() {
     // First, build a ParamDecoder:
     let param_decoder: ParamDecoder = ParamDecoderBuilder::new()
@@ -310,8 +319,8 @@ fn main() {
     // 1) Decode from the binary data in the GRIB sections:
     let param = param_decoder
         .from_grib() // -> ParamDecoderFromGrib
-        .discipline(Discipline::from_u8(0)) // or Discipline::from_u8(0)
-        .category(MeteorologicalProduct::from_u8(1)) //
+        .discipline(Product::from_u8(0)) // or Discipline::from_u8(0)
+        .category(MeteorologicalCategory::from_u8(1)) //
         .parameter_number(0)
         .build();
 
